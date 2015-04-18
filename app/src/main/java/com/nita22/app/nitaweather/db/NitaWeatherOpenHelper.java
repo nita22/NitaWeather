c84cfeda2b509d8c7a2babd174e97a378db49355
nita22 <nita510903569@gmail.com>
2015-04-18 19:46:13 +0800

    新增数据库帮助类

---
+++ app/src/main/java/com/nita22/app/nitaweather/db/NitaWeatherOpenHelper.java
@@ -0,0 +1,27 @@
+package com.nita22.app.nitaweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NitaWeatherOpenHelper extends SQLiteOpenHelper{
+
+    public static final String CREATE_PROVINCE = "create table Province( " + "id integer primary key autoincrement, " + "province_name text, " + "province_code text) ";
+    public static final String CREATE_CITY = "create table City( " + "id integer primary key autoincrement, " + "city_name text, " + "city_code text, " + "province_id integer) ";
+    public static final String CREATE_COUNTY = "create table County( " + "id integer primary key autoincrement, " + "county_name text, " + "county_code text, " + "city_id integer) ";
+
+    public NitaWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, name, factory, version);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+}
+
---
+++ app/src/main/java/com/nita22/app/nitaweather/model/Province.java
@@ -0,0 +1,33 @@
+package com.nita22.app.nitaweather.model;
+
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
